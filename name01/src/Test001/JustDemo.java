167530230595cfe0b68c9104fba032ade38a87b5
Administrator <1633235633@qq.com>
2019-04-17 15:06:07 +0800

    002

--- name01/src/Test001/JustDemo.java
+++ name01/src/Test001/JustDemo.java
@@ -2,6 +2,14 @@ package Test001;
 
 public class JustDemo {
     public static void main(String[] args) {
+        int[] score = new int[3];
+        score[2] =5;
+        score[1]=8;
+        score[0]=55;
+        for(int i=0;i<score.length;i++){
+            System.out.println(score[i]+"");
+        }
+    }
 
     }
-}
+
