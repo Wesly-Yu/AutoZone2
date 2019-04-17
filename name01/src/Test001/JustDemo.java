da8516086c75444bd80988db4d10c6ce0b4931ad
Administrator <1633235633@qq.com>
2019-04-17 15:31:01 +0800

    003

--- name01/src/Test001/JustDemo.java
+++ name01/src/Test001/JustDemo.java
@@ -2,14 +2,31 @@ package Test001;
 
 public class JustDemo {
     public static void main(String[] args) {
-        int[] score = new int[3];
-        score[2] =5;
-        score[1]=8;
-        score[0]=55;
-        for(int i=0;i<score.length;i++){
-            System.out.println(score[i]+"");
+        //冒泡排序算法
+        int[] a={6,32,55,21,8,1,78,14,66};
+        System.out.println("排序前的数组为：");
+        for(int n:a){
+            System.out.println(n+"");
+        }
+        System.out.println();
+        int temp;
+        for(int i=0;i<a.length-1;i++){
+            for(int j=0;j<a.length-i-1;j++){
+                if(a[j]>a[j+1]){
+                    temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+
+            }
+
+        }
+        System.out.println("冒泡排序后的数组为：");
+        for(int n:a){
+            System.out.println(n+"  ");
         }
     }
 
-    }
+
+}
 
