fa5e082f3bf969e9060e7b342c73712c2352e8e7
Administrator <1633235633@qq.com>
2019-04-19 10:07:53 +0800

    004

---
+++ name01/src/com/wesley/initdemo/InitDemoTest.java
@@ -0,0 +1,20 @@
+package com.wesley.initdemo;
+
+/**
+ *
+ *
+ */
+public class InitDemoTest {
+    //属性
+    String name;
+    int month;
+    double weight;
+    String species;
+    //方法
+    public  void run(){
+        System.out.println("小猫快跑");
+    }
+    public void eat(){
+        System.out.println("吃猫粮");
+    }
+}
