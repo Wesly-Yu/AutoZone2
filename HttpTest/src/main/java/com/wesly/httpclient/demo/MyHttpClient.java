8c1c71af67d33a50227cbf0576d304567751f7b6
Administrator <1633235633@qq.com>
2019-05-07 16:52:32 +0800

    016

--- HttpTest/src/main/java/com/wesly/httpclient/demo/MyHttpClient.java
+++ HttpTest/src/main/java/com/wesly/httpclient/demo/MyHttpClient.java
@@ -6,6 +6,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -18,13 +19,16 @@ public class MyHttpClient {
     public void test01() throws IOException {
         //存放结果
         String result;
+        String path;
         HttpGet get = new HttpGet("http://car.schouqin.gov.cn:8085/index");
         //client 用于执行get方法
         HttpClient client = new DefaultHttpClient();
         HttpResponse response = client.execute(get);
-        //getEntity 获取响应的所有内容；
+        //getEntity 获取响应的所有内容,转换为string
         result = EntityUtils.toString(response.getEntity(),Charset.defaultCharset());
-        System.out.println(result);
+        //获取当前执行文件所在的娄静
+        path =this.getClass().getResource(".").getPath();
+        System.out.println(path);
 
     }
 }
