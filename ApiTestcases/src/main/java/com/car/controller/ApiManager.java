7ca4cb3e06e24f3cd0175914d1db54d387ac033d
Administrator <1633235633@qq.com>
2019-05-15 11:39:20 +0800

    interface-004

--- ApiTestcases/src/main/java/com/car/controller/ApiManager.java
+++ ApiTestcases/src/main/java/com/car/controller/ApiManager.java
@@ -4,13 +4,10 @@ package com.car.controller;
 import com.car.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.extern.log4j.Log4j;
 import lombok.extern.log4j.Log4j2;
-import org.apache.catalina.LifecycleState;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,17 +16,15 @@ import java.util.Objects;
 
 @Log4j2
 @RestController//包含Controller和ReauqestBody两个方法
-@Api(value = "v1",description = "用户管理接口")//v1为设置的第一个路径前的参数
+@Api(value = "/",description = "用户管理接口")//v1为设置的第一个路径前的参数
 @RequestMapping("v1") //定义url模板,从v1字段开始查找访问路径,与下方的RequestMapping对应
 public class ApiManager {
 
     @Autowired  //配合Bean注解使用
     private SqlSessionTemplate template;
     @ApiOperation(value = "登陆接口",httpMethod = "POST")
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-    //是否登录成功返回布尔值
-    //HttpServletResponse  返回cookies
-    public Boolean login(HttpServletResponse response, @RequestBody User user){
+    @RequestMapping(value = "/login",method = RequestMethod.POST)//是否登录成功返回布尔值//HttpServletResponse  返回cookies
+    public Boolean login(HttpServletResponse response,@RequestBody User user){
         int res = template.selectOne("login",user);//查询数据库中是否有user
         Cookie cookie = new Cookie("login","true");
         response.addCookie(cookie);
@@ -43,7 +38,7 @@ public class ApiManager {
     }
     @ApiOperation(value = "添加用户",httpMethod = "POST")
     @RequestMapping(value = "/addUser",method = RequestMethod.POST)
-    public boolean addUser(HttpServletRequest request, @RequestBody User user){
+    public boolean addUser(HttpServletRequest request,@RequestBody User user){
         Boolean result = vervifyCookie(request);
         int defaultresult = 0;
         if(result!=null){
