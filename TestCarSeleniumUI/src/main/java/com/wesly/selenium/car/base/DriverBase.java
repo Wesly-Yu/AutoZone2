681f8c4ba38a840e0f4537a98fbb4b63b51491ba
Administrator <1633235633@qq.com>
2019-05-17 15:21:06 +0800

    UITest--001

--- TestCarSeleniumUI/src/main/java/com/wesly/selenium/car/base/DriverBase.java
+++ TestCarSeleniumUI/src/main/java/com/wesly/selenium/car/base/DriverBase.java
@@ -1,4 +1,7 @@
 package com.wesly.selenium.car.base;
 
+import org.openqa.selenium.WebDriver;
+
 public class DriverBase {
+    WebDriver driver;
 }
---
+++ TestCarSeleniumUI/src/main/java/com/wesly/selenium/car/cases/Login.java
@@ -0,0 +1,41 @@
+package com.wesly.selenium.car.cases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import javax.swing.*;
+import java.util.concurrent.TimeUnit;
+
+public class Login {
+    public WebDriver driver;
+    public void InitDriver(){
+        System.setProperty("webdriver.chrome,driver","C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver.exe");
+        driver = new ChromeDriver();
+        String url="http://car.schouqin.gov.cn:16002/index";
+        driver.get(url);
+        driver.manage().window().maximize();
+    }
+    public void loginCarManagerment() throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement username = driver.findElement(By.id("name"));
+        username.sendKeys("sj_sqr");
+        WebElement password =driver.findElement(By.id("password"));
+        password.sendKeys("a12345678");
+        WebElement login_btn = driver.findElement(By.xpath("/html/body/div/div[4]/form/button"));
+        login_btn.click();
+        Thread.sleep(3000);
+//        Actions actions = new Actions(driver);
+        driver.close();
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Login loginstep = new  Login();
+        loginstep.InitDriver();
+        loginstep.loginCarManagerment();
+
+    }
+}
