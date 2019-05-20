4b93db19ec82a7b9c2ddfaeb6157cb5e2d5ccfa4
Administrator <1633235633@qq.com>
2019-05-20 16:08:10 +0800

    UITest--012

--- TestCarSeleniumUI/src/main/java/com/wesly/selenium/car/business/LoginPro.java
+++ TestCarSeleniumUI/src/main/java/com/wesly/selenium/car/business/LoginPro.java
@@ -2,12 +2,14 @@ package com.wesly.selenium.car.business;
 
 import com.wesly.selenium.car.base.DriverBase;
 import com.wesly.selenium.car.operation.loginPagehandel;
+import org.testng.annotations.Test;
 
 public class LoginPro {
     public loginPagehandel lph;
     public LoginPro(DriverBase driver){
        lph = new loginPagehandel(driver);
     }
+    @Test
     public void login(String username,String password){
         lph.sendUserKey(username);
         lph.sendPassWordkey(password);
