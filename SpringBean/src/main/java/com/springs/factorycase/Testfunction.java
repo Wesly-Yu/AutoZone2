04c84e54a7a801741be1694b51f3d4f1257fcca1
Administrator <1633235633@qq.com>
2019-05-23 10:58:28 +0800

    spring_ioc -003

--- SpringBean/src/main/java/com/springs/factorycase/Testfunction.java
+++ SpringBean/src/main/java/com/springs/factorycase/Testfunction.java
@@ -2,10 +2,11 @@ package com.springs.factorycase;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.testng.annotations.Test;
 
 public class Testfunction {
-//    @Test
+    @Test
     public void demo1(){
         USerservice userservice = new UserServiceimp();
         userservice.sayHello();
@@ -21,6 +22,16 @@ public class Testfunction {
         //通过工厂获得类,隐藏了方法类.
         USerservice userservice = (USerservice) applicationContext.getBean("user");
         userservice.sayHello();
+
+    }
+    @Test
+    public void demo3(){
+        //使用Spring的工厂
+        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("f:\\applicationContext.xml");
+        //通过工厂获得类,隐藏了方法类.
+        USerservice userservice = (USerservice) applicationContext.getBean("user");
+        userservice.sayHello();
+
     }
 }
 
--- SpringBean/src/main/java/com/springs/factorycase/UserServiceimp.java
+++ SpringBean/src/main/java/com/springs/factorycase/UserServiceimp.java
@@ -1,7 +1,16 @@
 package com.springs.factorycase;
 
 public class UserServiceimp  implements USerservice{
+    String user;
     public void sayHello() {
         System.out.println("Hello word");
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getUser() {
+        return user;
     }
 }
