646417285b9a55ff2c26d5e95ef424261af4db69
Administrator <1633235633@qq.com>
2019-05-23 10:12:55 +0800

    spring_ioc -001

---
+++ SpringBean/src/main/java/com/springs/factorycase/UserInterface.java
@@ -0,0 +1,4 @@
+package com.springs.factorycase;
+
+public class UserInterface {
+}
--- TestCarSeleniumUI/src/main/java/com/wesly/selenium/car/method/GetLocator.java
+++ TestCarSeleniumUI/src/main/java/com/wesly/selenium/car/method/GetLocator.java
@@ -1,17 +1,27 @@
 package com.wesly.selenium.car.method;
-
 import org.openqa.selenium.By;
-
 import java.util.ResourceBundle;
 
-public class GetLocator {
-    public static By GetLocator(String key){
+
+public class getLocator {
+    public static By getLocator(String key){
         ResourceBundle resource = ResourceBundle.getBundle("data");
         String locator = resource.getString(key);
         String locatorType = locator.split(">")[0];
         String locatorValue = locator.split(">")[1];
         if (locatorType.equals("id")){
-
+            return By.id(locatorValue);
+        }else if (locatorType.equals("name")){
+            return By.name(locatorValue);
+        }else if (locatorType.equals("claname")){
+            return By.className(locatorValue);
+        }else if (locatorType.equals("xpath")){
+            return By.xpath(locatorValue);
+        }else if (locatorType.equals("css")){
+            return By.cssSelector(locatorValue);
+        }else if (locatorType.equals("linkText")){
+            return By.linkText(locatorValue);
         }
+        return By.tagName(locatorValue);
     }
 }
