6db57f26b9188ff4a268f60bd9c52e15cdad7dec
Administrator <1633235633@qq.com>
2019-07-04 16:34:51 +0800

    增加获取接口返回值的状态码--2

---
+++ HttpTest/src/main/java/com/wesly/httpclient/demo/User.java
@@ -0,0 +1,27 @@
+package com.wesly.httpclient.demo;
+
+public class User {
+    private String name;
+    private int money;
+
+    public User(String name, int money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+}
