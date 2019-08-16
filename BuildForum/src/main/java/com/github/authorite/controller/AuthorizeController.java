651691a47ab90f9d275494e3d74c82dea89aad47
Wesley-Yu <1633235633@qq.com>
2019-08-16 16:49:26 +0800

    buildloginpage--006

--- BuildForum/src/main/java/com/github/authorite/controller/AuthorizeController.java
+++ BuildForum/src/main/java/com/github/authorite/controller/AuthorizeController.java
@@ -1,15 +1,23 @@
 package com.github.authorite.controller;
 
 
+import com.github.authorite.dto.AccessTokenDTO;
+import com.github.authorite.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        githubProvider.getAccessToken(accessTokenDTO);
         return "login";
     }
 }
