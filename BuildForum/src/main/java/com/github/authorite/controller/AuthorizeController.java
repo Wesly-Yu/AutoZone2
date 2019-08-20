011311f480768502abb664b941a554266edc3d2e
Wesley-Yu <1633235633@qq.com>
2019-08-20 17:48:00 +0800

    移除不必要的文件--001

--- BuildForum/src/main/java/com/github/authorite/controller/AuthorizeController.java
+++ BuildForum/src/main/java/com/github/authorite/controller/AuthorizeController.java
@@ -1,12 +1,15 @@
 package com.github.authorite.controller;
 
 
+
+
 import com.github.authorite.dto.AccessTokenDTO;
 import com.github.authorite.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class AuthorizeController {
@@ -17,6 +20,10 @@ public class AuthorizeController {
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id("1c6819d3d068acda9639");
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setRedirect_uri("http://localhost:8090/callback");
         githubProvider.getAccessToken(accessTokenDTO);
         return "login";
     }
