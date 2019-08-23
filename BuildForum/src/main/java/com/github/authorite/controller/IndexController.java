c6e31685bea4f61ac5b2ca279d25bdf54e94cd40
Wesley-Yu <1633235633@qq.com>
2019-08-23 16:53:42 +0800

    移除不必要的文件--002

--- BuildForum/src/main/java/com/github/authorite/controller/IndexController.java
+++ BuildForum/src/main/java/com/github/authorite/controller/IndexController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
+
+    @GetMapping("/login")
     public String Index(){
         return "login";
     }
